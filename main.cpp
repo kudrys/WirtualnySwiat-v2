bfe8d730f0e93c002bd5f8601e418228dd2a0996
Kudrys <ryskuduk@gmail.com>
2016-11-21 15:35:32 +0100

    runda jedzenie kolejnego w kolejce, fix

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 void test(){
 
-    Swiat nowy(5,5);
+    Swiat nowy(5,1);
     //nowy.losowanieXY();
 
-    nowy.wsadzZwierzakaDoSwiata(1,'W');
-    nowy.wsadzZwierzakaDoSwiata(6,'L');
-    nowy.wsadzZwierzakaDoSwiata(11,'T');
-    nowy.wsadzZwierzakaDoSwiata(16,'Z');
-    nowy.wsadzZwierzakaDoSwiata(7,'L');
-    nowy.wsadzZwierzakaDoSwiata(12,'C');
+    nowy.wsadzZwierzakaDoSwiata(0,'D');
+    nowy.wsadzZwierzakaDoSwiata(1,'L');
+    nowy.wsadzZwierzakaDoSwiata(2,'W');
+    nowy.wsadzZwierzakaDoSwiata(3,'O');
+    nowy.wsadzZwierzakaDoSwiata(4,'Z');
+    //nowy.wsadzZwierzakaDoSwiata(6,'L');
     cout<<endl;
     //nowy.tura(nowy.organizmyTab[2][1]);   //do organizmyTab trzeba podawac odwrotnie X i Y
     cout<<endl<<"----WYNIK---"<<endl;
@@ -28,19 +28,19 @@ void test(){
 
 int main(){
 
-    //test();
-    cout<<"RYSZARD KUDUK 143271\n";
-    int x,y;
-    cout<<"podaj x:";
-    cin>>x;
-    cout<<"podaj y:";
-    cin>>y;
-
-    Swiat nowy(x,y);
-    nowy.losowanieXY();
-    cout<<endl;
-    nowy.kolejka.wypisz();
-    while(1){
-        nowy.runda();
-    }
+    test();
+//    cout<<"RYSZARD KUDUK 143271\n";
+//    int x,y;
+//    cout<<"podaj x:";
+//    cin>>x;
+//    cout<<"podaj y:";
+//    cin>>y;
+//
+//    Swiat nowy(x,y);
+//    nowy.losowanieXY();
+//    cout<<endl;
+//    nowy.kolejka.wypisz();
+//    while(1){
+//        nowy.runda();
+//    }
 }
